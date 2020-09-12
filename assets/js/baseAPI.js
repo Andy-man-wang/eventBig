28b8e964ffb42c0e0792629022f9f04ace9592a0
Andy-man-wang <2273857395@qq.com>
2020-09-12 08:32:00 +0800

    将index放入仓库

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,22 @@
 $.ajaxPrefilter(function(options){
 options.url="http://ajax.frontend.itheima.net"+options.url;
 
+if(options.url.indexOf('/my/')!==-1)
+{
+    options.headers={
+    Authorization: localStorage.getItem('token') || ''
+}
+}
+options.complete = function (res) {
+  // console.log(res);
+  console.log(res.responseJSON);
+
+  if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+      // 1.清除token的值
+      localStorage.removeItem('token')
+      // 2.跳转到登录页面
+      location.href = "login.html"
+  }
+}
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,71 @@
+$(function () {
+    var layer = layui.layer
+    getUserInfo()
+
+    // 点击退出按钮退出页面
+    $('#btnLogout').on("click", function () {
+        // 弹出弹出层
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 点击确定执行该回调函数
+            // 1.清除token的值
+            localStorage.removeItem('token')
+            // 2.跳转到登录页面
+            location.href = "login.html"
+            layer.close(index);
+        });
+
+    })
+
+
+
+})
+
+// 获取用户信息
+function getUserInfo() {
+    $.ajax ({
+        url: '/my/userinfo',
+        method: "GET",
+        success: function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            renderAvatar(res.data)
+        },
+        // complete: function (res) {
+        //     // console.log(res);
+        //     console.log(res.responseJSON);
+
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+        //         // 1.清除token的值
+        //         localStorage.removeItem('token')
+        //         // 2.跳转到登录页面
+        //         location.href = "login.html"
+        //     }
+        // }
+
+    })
+
+
+}
+// 封装renderAvatar渲染用户名和用户头像
+function renderAvatar(user) {
+    //  console.log(user);
+    // 渲染用户名
+    var name = user.nickname || user.username;
+    $('#welcome').html(name)
+    var one = name[0].toUpperCase()
+    $('.text-avatar').html(one)
+    // 渲染用户头像
+    if (user.user_pic !== null) {
+        $(".layui-nav-img").attr('src', user.user_pic)
+        $(".layui-nav-img").show()
+        $('.text-avatar').hide()
+    } else {
+        $('.text-avatar').show()
+        $(".layui-nav-img").hide()
+    }
+
+
+
+}
