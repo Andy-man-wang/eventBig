604e13117deb2e8ec8eda859ec9643c4b46c60f6
Andy-man-wang <2273857395@qq.com>
2020-09-14 17:06:51 +0800

    图片裁剪完成

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,9 +1,9 @@
 $(function () {
   var p = {
-    pagenum: 1,
-    pagesize: 2,
-    cate_id: '',
-    state: ''
+    pagenum: 1, //页码值，默认请求第一页的数据
+    pagesize: 2,//每页显示几条数据
+    cate_id: '',//文章分类的id
+    state: '' //文章的发布状态
   }
   var layer = layui.layer
   var form = layui.form
@@ -15,12 +15,18 @@ $(function () {
       type: 'GET',
       data: p,
       success: function (res) {
- 
-        var inner = template('tpl-table', res)
-        $('tbody').html(inner)
+
+        // res.data.forEach(function(item){
+        //   console.log(item);
+        //   item.pub_date=dayjs(item.pub_date).format('YYYY-MM-DD HH:MM:SS')
+        // })
+        var tableHtml = template('tpl-table', res)
+        $('tbody').html(tableHtml)
+        // console.log(res);
+        renderPage (res.total)
+       
       }
     })
-
   }
 
   initCate()
@@ -51,6 +57,62 @@ $('#form-search').on('submit',function (e) {
   p.state = $('[name=state]').val();
   initTable()
 })
+
+
+// 创建renderPage函数渲染分页
+function renderPage (total){
+  // console.log(total);
+  // 引入layui中的分页
+  var laypage = layui.laypage;
+  laypage.render({
+    elem:'pageBox',
+    count:total,
+    limit:p.pagesize,
+    curr:p.pagenum,
+    layout:['count','limit','prev', 'page', 'next','skip'],
+    limits:[2,3,5,10],
+    jump: function(obj, first){
+      //obj包含了当前分页的所有参数，比如：
+    // console.log(obj.curr) //得到当前页，以便向服务端请求对应页的数据。
+    // console.log(obj.limit); //得到每页显示的条数
+    p.pagenum=obj.curr
+    p.pagesize=obj.limit
+    if(!first){
+      initTable()
+    }
+    }
+  })
+}
+
+// 点击删除按钮实现删除页面的操作
+$('body').on('click','.btn-delete',function(){
+  // alert('123')
+  var id = $(this).attr('data-id')
+  var len = $('.btn-delete').length
+  layer.confirm('确认删除?', {icon: 3, title:'提示'}, function(index){
+    //do something
+    
+    $.ajax({
+      url:'/my/article/delete/'+id,
+      type:'get',
+      
+      success:function(res){
+        if(status=== 0){
+          return layer.msg('删除失败');
+        }
+        layer.msg("删除成功")
+        
+      if(len===1){
+        p.pagenum=p.pagenum===1 ? 1 : p.pagenum-1
+      }
+      initTable()
+      }
+    })
+    
+    layer.close(index);
+  });
+})
+
 
 
 
---
+++ assets/js/article/art_pub.js
@@ -0,0 +1,48 @@
+$(function () {
+  var layer = layui.layer
+  var form = layui.form
+  // 初始化富文本编辑器
+  initEditor()
+  initCate()
+  // 获取文章分类并渲染
+  function initCate() {
+    $.ajax({
+      url: '/my/article/cates',
+      type: 'get',
+      success: function (res) {
+        // console.log(res);
+        if (res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        var strHtml = template('tpl-cate', res)
+        $('[name=cate_id]').html(strHtml)
+        form.render()
+      }
+    })
+  }
+
+  // 图片裁剪
+    // 1. 初始化图片裁剪器
+    var $image = $('#image')
+    // 2. 裁剪选项
+    var options = {
+      aspectRatio: 400 / 280,
+      preview: '.img-preview'
+    }
+    // 3. 初始化裁剪区域
+    $image.cropper(options)
+
+
+  // 为选择封面添加点击事件
+  $('#btnChooseImage').on('click',function(){
+    $('#filter').click()
+  })
+  $('#filter').change(function(e){
+    var file = e.target.files[0]
+    var newImgURL = URL.createObjectURL(file)
+    $image
+   .cropper('destroy')      // 销毁旧的裁剪区域
+   .attr('src', newImgURL)  // 重新设置图片路径
+   .cropper(options)        // 重新初始化裁剪区域
+  })
+})
